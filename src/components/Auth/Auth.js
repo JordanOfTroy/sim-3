f8943249caa283f96d26613165d4283eea00d9ac
Jordan Smithson <jordantroysmithson@gmail.com>
2018-09-14 11:40:35 -0600

    username and password inputs created and functional with local state

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,46 @@
 import React, {Component} from 'react' 
 
 class Auth extends Component {
+
+  constructor (props) {
+    super(props) 
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+    this.handelInput = this.handelInput.bind(this)
+  }
+
+  handelInput (e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render () {
+    console.log(this.state)
     return (
       <div>
         <h1>Auth</h1>
+        <div>
+          <input
+          name = 'username'
+          onChange = {this.handelInput} 
+          placeholder = 'Please Enter UserName'
+          value = {this.state.username}
+          type="text"/>
+          <input
+          name = 'password'
+          onChange = {this.handelInput} 
+          placeholder = 'Please Enter Password'
+          value = {this.state.password}
+          type="text"/>
+        </div>
+        <div>
+          <button>Login, My Dude!</button>
+          <button>Register Bro!</button>
+        </div>
       </div>
     )
   }
