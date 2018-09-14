eb8509bcaa337b80e27b27acc23a9feb811e2114
Jordan Smithson <jordantroysmithson@gmail.com>
2018-09-14 11:28:34 -0600

    conditional rendering and nav bar links set

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,34 @@
 import React from 'react' 
+import {withRouter} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-function Nav () {
-  return (
-    <div>
-      <h1>Nav</h1>
-    </div>
-  )
+function Nav (props) {
+  let {pathname} = props.location
+ 
+   {
+     if (pathname === '/') {
+      return (
+        <h1>nav</h1>
+      )
+     } else {
+      return (
+        <div>
+          <Link to = '/dashboard'>
+            <button>Home</button>
+          </Link>
+          <Link to = '/new'>
+            <button>New Post</button>
+          </Link>
+          <Link to = '/'>
+            <button>Logout</button>
+          </Link>
+        </div>
+      )
+     }
+   }
+
 }
 
-export default Nav
+export default withRouter(Nav) 
+
 
